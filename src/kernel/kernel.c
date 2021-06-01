160a4bf56fbb479494d3d2f54295fe965eea80b4
xing1357 <71458491+xing1357@users.noreply.github.com>
2021-06-01 10:39:46 -0400

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -7,31 +7,6 @@
 #include "fs/initrd.h"
 #include "multiboot.h"
 
-void find_files(){
-	int i = 0;
-	struct dirent *node = 0;
-	while ( (node = readdir_fs(fs_root, i)) != 0)
-	{
-	  print_string("Found file ");
-	  print_string(node->name);
-	  fs_node_t *fsnode = finddir_fs(fs_root, node->name);
-
-	  if ((fsnode->flags&0x7) == FS_DIRECTORY)
-	    print_string("\n\t(directory)\n");
-	  else
-	  {
-	    print_string("\n\t contents: \"");
-	    char buf[256];
-	    uint32 sz = read_fs(fsnode, 0, 256, buf);
-	    int j;
-	    for (j = 0; j < sz; j++)
-	      print_char(buf[j]);
-
-	    print_string("\"\n");
-	  }
-  	i++;
-	}
-}
 
 void kernel_entry(struct multiboot *mboot_ptr)
 {
@@ -43,7 +18,6 @@ void kernel_entry(struct multiboot *mboot_ptr)
   	print_string("IDT Initialised\n");
   	fs_root = initialise_initrd (initrd_location);
   	print_string("Loaded Initial RAMDISK\n");
-  	find_files();
   	print_string("Welcome to SimpleOS!\nPlease enter a command\n");
   	print_string("Enter 'help' for commands\n");
   	launch_shell(0);
