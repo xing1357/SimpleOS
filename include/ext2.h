e710a3b4efeb2376185cfc210d46453bbcd4d418
xing1357 <71458491+xing1357@users.noreply.github.com>
2021-11-29 18:22:59 -0600

    Added some stuff to ext2 fs

--- include/ext2.h
+++ include/ext2.h
@@ -11,10 +11,21 @@ typedef struct ext2_superblock {
     uint32 sb_block_num;
     uint32 block_size;
     uint32 fragment_size;
+    uint32 fragments_per_group;
     uint32 inodes_per_group;
 }ext2_superblock;
 
+typedef struct ext2_bgdt {
+    uint32 blk_addr_blk_bitmap;
+    uint32 blk_addr_inode_bitmap;
+    uint32 inode_table_start;
+    uint32 unalloc_blocks;
+    uint32 unalloc_inodes;
+    uint32 num_of_dirs;
+}ext2_bgdt;
+
 ext2_superblock sb;
+ext2_bgdt bgdt;
 
 void read_superblock();
 void dump_fs_info();
--- src/ext2.c
+++ src/ext2.c
@@ -21,7 +21,26 @@ void read_superblock(){
     sb.unalloc_inodes = (uint32)unalloc_inodes;   
     int sb_block_num = toDeci(superblock[5], 16);
     sb.sb_block_num = (uint32)sb_block_num;
+    int block_size = toDeci(superblock[6], 16);
+    sb.block_size = 1024 << (uint32)block_size;
+    int fragment_size = toDeci(superblock[7], 16);
+    sb.fragment_size = 1024 << (uint32)fragment_size;
+    int fragments_per_group = toDeci(superblock[8], 16);
+    sb.fragments_per_group = (uint32)fragments_per_group;
+    int inodes_per_group = toDeci(superblock[9], 16);
+    sb.inodes_per_group = (uint32)inodes_per_group;
          
+}
+
+uint32 determine_blk_group(uint32 inode){
+    uint32 block_group = (inode - 1) / sb.inodes_per_group;
+    return block_group;
+}
+
+uint32 get_inode_block(uint32 inode){
+    int index = (inode - 1) / sb.inodes_per_group;
+    uint32 containing_block = (index * 128) / sb.block_size;
+    return containing_block;
 }
 
 void dump_fs_info(){
@@ -31,4 +50,6 @@ void dump_fs_info(){
     printf("\n Unallocated Blocks: %x", sb.unalloc_blocks);
     printf("\n Unallocated Inodes: %x", sb.unalloc_inodes);
     printf("\n Superblock Address: %x", sb.sb_block_num);
+    printf("\n Block Size: %x", sb.block_size);
+    printf("\n Fragment Size: %x", sb.fragment_size);
 }
